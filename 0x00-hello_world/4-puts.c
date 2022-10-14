39aa341e46eb18f063db46e4dcac4c1d8c53130c
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-14 15:25:33 -0700

    Scripting and Compiling in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+* main - Entry point for our c program
+*
+* Description: Prints word to standard output.
+* Return: Returns success
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
