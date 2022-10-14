676951411074cdc84e0245c1076ab1f5c7b06569
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-14 15:49:29 -0700

    Scripting and Compiling in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of char: %lu byte(s)", (unsigned long)sizeof(c));
-printf("Size of int: %lu byte(s)", (unsigned long)sizeof(i));
-printf("Size of long int: %lu byte(s)", (unsigned long)sizeof(li));
-printf("Size of long long int: %lu byte(s)", (unsigned long)sizeof(lli));
-printf("Size of float: %lu byte(s)", (unsigned long)sizeof(f));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
