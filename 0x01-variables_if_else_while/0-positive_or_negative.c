b8c1acae6b3866f743ec647fba7dc2a8abee9f9c
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-14 17:27:42 -0700

    Variables and Loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 return (0);
 }
