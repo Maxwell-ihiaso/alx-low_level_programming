6914f7d8df62f9f6e60b27b7aa7b829a202daef1
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-14 18:44:50 -0700

    Variables and Loops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point for function
+*
+* Description: Prints single base 10 numbers
+* without using printf or put
+* Return: return success
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar((n % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
