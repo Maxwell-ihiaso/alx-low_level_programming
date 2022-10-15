d20459699d6c05949f73561ed427ca92c9e94d6c
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-14 18:05:38 -0700

    Variables and Loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point for function
+*
+* Description: This program will assign a random number
+* to variable n
+* Return: Returns  success
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
