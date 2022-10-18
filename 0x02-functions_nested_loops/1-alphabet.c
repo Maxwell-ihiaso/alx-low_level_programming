31a39065208af584badcb335a064205454bb73cc
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-18 14:28:37 -0700

    C functions and header construct

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point for function
+ *
+ * Description: This program will assign a random number
+ * to variable n
+ * Return: Returns  success
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
