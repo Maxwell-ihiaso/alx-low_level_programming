5c1d87d86113decb586dc6756bf5dc9c12fa2405
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-18 14:19:21 -0700

    C functions and custom header

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - entry point for function
+*
+* Description: This program will assign a random number
+* to variable n
+* Return: Returns  success
+*/
+int main(void)
+{
+char c[] = "_putchar";
+int i = 0;
+
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
