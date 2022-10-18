de4dfd01b8787e2e5a7d147c20b7208aa4c2db77
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-18 14:38:28 -0700

    C functions and header construct

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point for function
+ *
+ * Description: This program will assign a random number
+ * to variable n
+ * Return: Returns  success
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (i < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+}
