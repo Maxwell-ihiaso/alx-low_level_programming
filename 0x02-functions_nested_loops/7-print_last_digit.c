9ece7d6ed9c32e7645d09415fd560992688da7bd
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-19 09:44:37 -0700

    C functions and header usage

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point for function
+ * @c: Parameter
+ *
+ * Description: This program will assign a random number
+ * to variable n
+ * Return: Returns  success
+ */
+int print_last_digit(int c)
+{
+int n = c % 10;
+return (n);
+}
