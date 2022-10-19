7a622dd2eb66b25109bf985de64a85b27cd09e35
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-19 06:32:40 -0700

    C functions and header usage

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -19,6 +19,6 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-}
 i++;
+}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for case sensitivity
+ *
+ * Description: Return 1 for lower case letters 
+ * and 0 for otherwise
+ * Return: always returns 0
+ */
+int _islower(int c)
+{
+  char c;
+}
