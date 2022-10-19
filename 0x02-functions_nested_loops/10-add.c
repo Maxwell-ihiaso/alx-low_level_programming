161d00ef16c9cc33cd9f2664ffa6220f059bf141
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-19 10:24:09 -0700

    C functions and header usage

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,7 +7,7 @@
  *
  * Description: Prints every minute in the day
  */
-void add(int a, int b)
+int add(int a, int b)
 {
 return (a + b);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,15 @@
 int print_last_digit(int c)
 {
 int n = c % 10;
+if (n >= 0)
+{
 _putchar(n + '0');
 return (n);
+}
+else
+{
+n *= -1
+_putchar(n + '0');
+return (n);
+}
 }
