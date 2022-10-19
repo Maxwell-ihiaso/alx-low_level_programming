eb8348f245daf52078627ce2fa0f4233e374cb5d
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-19 07:47:32 -0700

    C functions and header usage

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for case sensitivity
+ * @c: expect a value from the user
+ * Description: Return 1 for lower case letters
+ * and 0 for otherwise
+ * Return: always returns 0
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+_putchar('\n');
+return (0);
+}
