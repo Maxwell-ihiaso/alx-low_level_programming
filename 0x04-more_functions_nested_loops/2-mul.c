284aa54277c446ff441e49e2eef8a835b42362ea
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-20 15:26:10 -0700

    C Nested Functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - cheecks for integers
+ * @a: params integer
+ * @b: params integer
+ *
+ * Description: Checks for valid numbers
+ * Return: returns success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
