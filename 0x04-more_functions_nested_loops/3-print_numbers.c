8a47628a4409e2e0ba3ea67993e984b1b9ee35a6
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-20 15:30:52 -0700

    C Nested Functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * Description: Print numbers
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
