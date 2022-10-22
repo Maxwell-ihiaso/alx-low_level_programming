ca4e0c3dfb74866c760df84440974d5e50b8828d
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-21 21:42:21 -0700

    Debugging is interesting

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -23,3 +23,4 @@ void print_square(int size);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
