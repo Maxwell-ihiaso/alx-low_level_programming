3cda84c8e63f1a8695ac2a73a2e4a74ff1dc6b9e
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-24 19:56:31 -0700

    Pointers stuff

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * void reset_to_98 -  function that resets to 98.
  * @n: pointer
- * Description; Resest to 98
+ * Description: Resest to 98
  * Return: Always 0.
  */
 void reset_to_98(int *n)
