a90bc706ec343d4fcf6116b123160e4030a8b4a3
Maxwell-ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2022-10-24 20:24:51 -0700

    Pointers stuff

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - print a string length
+ * @s: parameter
+ * Description: Prints a string length
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char Str[1000];
+	int i;
+
+	for (i = 0; *s != '\0'; ++i);
+
+	return (i);
+}
